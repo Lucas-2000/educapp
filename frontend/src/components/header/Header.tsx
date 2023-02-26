bc289f84fc2f6641c5c940fd1aa0ab127bb7cc99
jnsneres <jonasneres717@gmail.com>
2023-02-26 12:51:58 -0300

    navbar atualizada

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Title } from "../title/Title";
 import { Link } from "../link/Link";
+import { User, SignIn, DotsThreeOutlineVertical, X } from "phosphor-react";
 
 export const Header = () => {
+  const [state, setState] = useState("");
+  const [icon, setIcon] = useState(true);
+ 
+ const toogleAccordion = () => {
+    setState(state === "" ? "open" : "");
+    setIcon(!icon);
+ }
+
   return (
     <header className="header">
       <Title>EducApp</Title>
-      <nav>
-        <ul className="navigation">
-          <li>
-            <Link path="/login">Login</Link>
+        <ul className={`navigation ${state}`}>
+        <li>
+            <a href="/#">Home</a>
+          </li>
+        <li>
+            <a href="/#">Planos</a>
+          </li>
+        <li>
+          <a href="/#">FAQ</a>
           </li>
           <li>
-            <Link path="/register">Cadastrar</Link>
           </li>
         </ul>
-      </nav>
+        
+        <div className="main">
+        <a href="/login"><User size={32} />Login</a>
+        <a href="/register"><SignIn size={32} />Cadastre-se</a>
+          <div onClick={toogleAccordion} className={`menu-icon`} >
+            {icon ?
+            <DotsThreeOutlineVertical size={32} /> : <X size={32} />} </div>
+        </div>
+        
     </header>
   );
 };
