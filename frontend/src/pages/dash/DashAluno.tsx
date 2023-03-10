003aa54dc0d1569e98551a31b80162ab88585bef
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-03-10 15:03:26 -0300

    Calendário iniciado

--- frontend/src/pages/dash/DashAluno.tsx
+++ frontend/src/pages/dash/DashAluno.tsx
@@ -12,6 +12,54 @@ export const DashAluno = ({ isAluno }: ISidebarProps) => {
       <div className="dash-container">
         <Title>Bem vindo, Aluno</Title>
         <Carousel isAluno={isAluno} />
+        <div className="calendar-container">
+          <div className="calendar">
+            <div className="calendar-header">
+              <span className="month-picker">February</span>
+              <div className="year-picker">
+                <span className="year-change">
+                  <pre>&lsaquo;</pre>
+                </span>
+                <span className="year">2023</span>
+                <span className="year-change">
+                  <pre>&rsaquo;</pre>
+                </span>
+              </div>
+            </div>
+            <div className="calendar-body">
+              <div className="calendar-week-day">
+                <div>Dom</div>
+                <div>Seg</div>
+                <div>Ter</div>
+                <div>Qua</div>
+                <div>Qui</div>
+                <div>Sex</div>
+                <div>Sab</div>
+              </div>
+              <div className="calendar-days">
+                <div>
+                  1<span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
